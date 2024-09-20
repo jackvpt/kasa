732f8c1a1170194cfe2def87bd53ac17e146059b
Jacques Verpoest <jacques.verpoest@gmail.com>
2024-09-20 11:33:48 +0200

    Carrousel

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -6,29 +6,30 @@ export default function Collapse({ item, text }) {
   const [textActive, setTextActive] = useState(false)
   let textList = []
 
-  typeof text === "string" ? textList[0]=text : textList=text
+  typeof text === "string" ? (textList[0] = text) : (textList = text)
 
   return (
     <article className="article__collapse">
-      <div className="collapse__item" onClick={() => setTextActive(!textActive)}>
-        <div> {item}</div>
+      <div className="collapse__item">
+        <h2>{item}</h2>
         <img
           className={textActive ? "collapse-arrow-down" : "collapse-arrow-up"}
           src={collapseArrow}
           alt="collapse arrow"
           width="24"
           height="24"
+          onClick={() => setTextActive(!textActive)}
         ></img>
       </div>
       {textActive && (
-        <div className="collapse__text"><ul>
-          {textList.map((element) => (
-            <li>{element}</li>
-          ))}
+        <div className="collapse__text">
+          <ul>
+            {textList.map((element) => (
+              <li>{element}</li>
+            ))}
           </ul>
         </div>
       )}
     </article>
   )
 }
-
--- src/pages/Accomodation/Accomodation.jsx
+++ src/pages/Accomodation/Accomodation.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import "./Accomodation.scss"
 import { accomodations } from "../../data/kasa.js"
@@ -6,23 +6,65 @@ import AccomodationTag from "../../components/AccomodationTag/AccomodationTag.js
 import star_inactive from "../../images/rating_start_inactive.png"
 import star_active from "../../images/rating_star_active.png"
 import Collapse from "../../components/Collapse/Collapse.jsx"
+import arrow_back from "../../images/arrow_back.png"
+import arrow_forward from "../../images/arrow_forward.png"
 
-function Accomodation() {
+export default function Accomodation() {
   const parameters = useParams()
   const accomodation = accomodations.find(
     (element) => element.id === parameters.id
   )
+
+  const [pictureDisplayed, setPictureDisplayed] = useState(0)
+
+  const nmberOfPictures = accomodation.pictures.length
+
+  // useEffect(() => {
+  //   if (pictureDisplayed>=nmberOfPictures){
+  //     setPictureDisplayed(0)
+  //   }
+  //   if (pictureDisplayed<0){
+  //     setPictureDisplayed(nmberOfPictures)
+  //   }
+  // }, [pictureDisplayed])
+
+  function pictureBack() {
+    pictureDisplayed - 1 < 0
+      ? setPictureDisplayed(nmberOfPictures - 1)
+      : setPictureDisplayed(pictureDisplayed - 1)
+  }
+  function pictureForward() {
+    pictureDisplayed + 1 >= nmberOfPictures
+      ? setPictureDisplayed(0)
+      : setPictureDisplayed(pictureDisplayed + 1)
+  }
 
   return (
     <React.Fragment>
       <section className="container__accomodation">
         <div className="container__carrousel">
           <img
-            src={accomodation.cover}
-            alt={accomodation.description}
+            className="accomodation__picture"
+            src={accomodation.pictures[pictureDisplayed]}
+            alt={accomodation.title}
             width="1240"
             height="415"
           ></img>
+          <img
+            className="picture__arrow_back"
+            src={arrow_back}
+            alt="précédent"
+            onClick={pictureBack}
+          ></img>
+          <img
+            className="picture__arrow_forward"
+            src={arrow_forward}
+            alt="suivante"
+            onClick={pictureForward}
+          ></img>
+          <p className="picture__number">
+            {pictureDisplayed + 1}/{nmberOfPictures}
+          </p>
         </div>
         <div className="container__description">
           <div className="container__description-location">
@@ -44,25 +86,28 @@ function Accomodation() {
         <div className="container__tags-rating">
           <div className="container__tags">
             {accomodation.tags.map((tag, index) => (
-              <p>
+              <div key={index}>
                 <AccomodationTag tag={tag} />
-              </p>
+              </div>
             ))}
           </div>
           <div className="container__rating">
             {[1, 2, 3, 4, 5].map((idx) => (
-                <img src={idx < accomodation.rating ? star_active : star_inactive} alt="rating" wdith="24" height="24"></img>
+              <img
+                key={idx}
+                src={idx < accomodation.rating ? star_active : star_inactive}
+                alt="rating"
+                wdith="24"
+                height="24"
+              ></img>
             ))}
           </div>
         </div>
         <div className="container__details">
-            <Collapse item="Description" text={accomodation.description} />
-            <Collapse item="Equipements" text={accomodation.equipments} />
-
+          <Collapse item="Description" text={accomodation.description} />
+          <Collapse item="Equipements" text={accomodation.equipments} />
         </div>
       </section>
     </React.Fragment>
   )
 }
-
-export default Accomodation
